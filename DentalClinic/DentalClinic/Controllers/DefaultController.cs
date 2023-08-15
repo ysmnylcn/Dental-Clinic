b43943de0582c28e85f297c831870ea0d795adda
Yasemin Yalçın <yasemin.yalcin1@st.uskudar.edu.tr>
2023-08-15 20:11:51 +0300

    Dental Clinic
    Diş hekimlerinin hasta takibi yapıp tedavi süreçlerini kaydedebildiği bir portal. MVC .Net ile geliştirilmiş bir web sitesi projesidir.

---
+++ DentalClinic/DentalClinic/Controllers/DefaultController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DentalClinic.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        public ActionResult Appointments()
+        {
+            return View();
+        }
+        public ActionResult Users()
+        {
+            return View();
+        }
+        public ActionResult Dentists()
+        {
+            return View();
+        }
+        public ActionResult Patients()
+        {
+            return View();
+        }
+        public ActionResult Treatments()
+        {
+            return View();
+        }
+        public ActionResult Diseases()
+        {
+            return View();
+        }
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
---
+++ DentalClinic/DentalClinic/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DentalClinic.MvcApplication" Language="C#" %>
